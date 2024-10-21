9603984382d39b4f8ca6fe0ec7b655270c9a547e
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-10-21 16:31:46 +0200

    Add specific error pages to catch in mobile browser

---
+++ src/app/error.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import * as Sentry from '@sentry/nextjs'
+import NextError from 'next/error'
+import { useEffect } from 'react'
+
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string }
+}) {
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <h1>An error occurred in the application</h1>
+      <NextError statusCode={0} />
+    </div>
+  )
+}
